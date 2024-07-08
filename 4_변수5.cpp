0c04f0a3c822837a0a52c942aeb86c3e6a7e86c9
Chansik Yun <chansigi@gmail.com>
2024-07-08 14:50:00 +0900

    .

---
+++ 4_변수5.cpp
@@ -0,0 +1,62 @@
+// 4_변수5.cpp
+
+#include <iostream>
+using namespace std;
+
+// typedef
+// : 기존 타입의 별칭을 만들 수 있습니다.
+//  => 변수를 만들듯이 타입의 별칭을 만들 수 있습니다.
+
+#if 0
+int main()
+{
+    typedef int ARR3[3];
+
+    // int x[3] = { 10, 20, 30 };
+    ARR3 x = { 10, 20, 30 };
+    x[0] = 100;
+    x[1] = 200;
+    x[2] = 300;
+
+    for (int i = 0; i < 3; i++) {
+        printf("%d\n", x[i]);
+    }
+
+    typedef int(*PARR3)[3];
+    PARR3 p = &x;
+}
+#endif
+
+// Type aliasing의 새로운 문법이 도입되었습니다.
+// => C++11
+
+// using
+// 1) namespace
+//   - using 선언
+//       using std::cout;
+//       using std::endl;
+//   - using 지시어
+//       using namespace std;
+
+// 2) C++11, Type 별칭
+
+int main()
+{
+    // typedef int ARR3[3];
+    using ARR3 = int[3];
+
+    // int x[3] = { 10, 20, 30 };
+    ARR3 x = { 10, 20, 30 };
+    x[0] = 100;
+    x[1] = 200;
+    x[2] = 300;
+
+    for (int i = 0; i < 3; i++) {
+        printf("%d\n", x[i]);
+    }
+
+    // typedef int(*PARR3)[3];
+    using PARR3 = int(*)[3];
+
+    PARR3 p = &x;
+}
