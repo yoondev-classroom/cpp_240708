8a4e09d7746728706094a5f78493b1bb5f0c032e
Chansik Yun <chansigi@gmail.com>
2024-07-08 13:43:32 +0900

    .

---
+++ 4_변수.cpp
@@ -0,0 +1,32 @@
+// 4_변수.cpp
+#include <iostream>
+using namespace std;
+
+// 사용자 정의 타입
+struct Complex {
+    int re;
+    int im;
+};
+
+union Data {
+    int a;
+    char c;
+};
+
+enum State {
+    RUNNING,
+    STOPPED
+};
+
+int main()
+{
+    struct Complex c1;
+    union Data d1;
+    enum State s;
+
+    // C++에서는 struct, union, enum을 명시하지
+    // 않아도 됩니다.
+    Complex c2;
+    Data d2;
+    State s2;
+}
