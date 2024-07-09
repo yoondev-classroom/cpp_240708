c7c7cb799231e7c8299f947eec4a0f2ba22c9c23
Chansik Yun <chansigi@gmail.com>
2024-07-09 13:35:38 +0900

    .

--- 9_레퍼런스3.cpp
+++ 9_레퍼런스3.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 
+#if 0
 // void print_array(int x[3])
 void print_array(int* x)
 {
@@ -49,4 +50,59 @@ int main()
     // print_array(&x[0])
 
     // std::cout << x[0] << std::endl; // ?
+}
+#endif
+
+// 핵심
+// - C 에서 배열을 인자로 전달받는 함수를 만들 때,
+//   함수에서는 배열의 첫번째 원소의 주소가 전달되기 때문에,
+//   전달된 배열의 길이를 알 수 없습니다.
+//  => 추가적인 인자로 길이를 전달받습니다.
+#if 0
+void print_array(int* x, size_t n)
+{
+    for (int i = 0; i < n; ++i) {
+        std::cout << x[i] << std::endl;
+    }
+}
+
+int main()
+{
+    int x[5] = { 10, 20, 30 };
+    print_array(x, sizeof(x) / sizeof(x[0]));
+}
+#endif
+
+#if 0
+void print_array(int (&r)[5])
+{
+    for (int i = 0; i < sizeof(r) / sizeof(r[0]); i++) {
+        std::cout << r[i] << std::endl;
+    }
+}
+
+void print_array(int (&r)[3])
+{
+    for (int i = 0; i < sizeof(r) / sizeof(r[0]); i++) {
+        std::cout << r[i] << std::endl;
+    }
+}
+#endif
+
+template <typename TYPE>
+inline void print_array(TYPE& r)
+{
+    for (int i = 0; i < sizeof(r) / sizeof(r[0]); i++) {
+        std::cout << r[i] << std::endl;
+    }
+}
+
+int main()
+{
+    int x[3] = { 10, 20, 30 };
+
+    // int(&r)[5] = x;
+    print_array(x);
+
+    // std::cout << sizeof(r) << std::endl;
 }
