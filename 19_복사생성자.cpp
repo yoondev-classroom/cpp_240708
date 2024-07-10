ee89e7aad909102fa8926c59d3a3ae2035886e7a
Chansik Yun <chansigi@gmail.com>
2024-07-10 14:27:14 +0900

    .

---
+++ 19_복사생성자.cpp
@@ -0,0 +1,49 @@
+// 19_복사생성자.cpp
+#include <iostream>
+using namespace std;
+
+// 1. 사용자가 생성자를 1개도 제공하지 않으면
+//  컴파일러는 아무일도 하지 않는 기본 생성자를 제공합니다.
+
+// 2. 사용자가 생성자를 1개라도 제공하면,
+//   컴파일러는 기본 생성자를 제공하지 않습니다.
+
+// 3. 사용자가 자신과 동일한 타입의 객체를 인자로 전달받는
+//   생성자(복사 생성자)를 제공하지 않는다면,
+//   컴파일러는 멤버를 복사하는 복사 생성자를 제공합니다.
+
+class Point {
+    int x;
+    int y;
+
+public:
+    Point(int a, int b)
+        : x { a }
+        , y { b }
+    {
+    }
+
+    Point(const Point& rhs) // 복사 생성자
+        : x { rhs.x }
+        , y { rhs.y }
+    {
+        std::cout << "Point(const Point&)" << std::endl;
+    }
+
+    void Print()
+    {
+        std::cout << x << ", " << y << std::endl;
+    }
+};
+
+int main()
+{
+    Point pt1 { 10, 20 };
+    pt1.Print();
+
+    Point o1 = pt1;
+    Point o2(pt1);
+    Point o3 = { pt1 };
+    Point other { pt1 };
+    other.Print();
+}
