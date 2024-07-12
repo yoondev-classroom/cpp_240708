ed7cc158daaa67db6115f19e624b9243dd720957
Chansik Yun <chansigi@gmail.com>
2024-07-12 13:32:03 +0900

    .

--- 30_연산자오버로딩3.cpp
+++ 30_연산자오버로딩3.cpp
@@ -2,6 +2,37 @@
 #include <iostream>
 using namespace std;
 
+namespace xstd {
+class istream {
+public:
+    istream& operator>>(int& n)
+    {
+        scanf("%d", &n);
+        return *this;
+    }
+
+    istream& operator>>(double& n)
+    {
+        scanf("%lf", &n);
+        return *this;
+    }
+};
+
+istream cin;
+
+}
+
 int main()
 {
+    int a;
+    int b;
+
+    xstd::cin >> a >> b;
+    std::cout << a << ", " << b << std::endl;
+
+    // int n;
+    // std::cin >> n;
+    // => std::cin.operator(n)
+    // => std::cin.operator(int&)
+    // std::cout << n << std::endl;
 }
